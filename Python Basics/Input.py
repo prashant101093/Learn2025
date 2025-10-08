c5df831fb1d0284edbcaaf4e06a6da1ec99cec37
Prashant Kumar <prashant.v.kumar10@gmail.com>
2025-10-08 21:47:50 +0530

    code added

---
+++ Python Basics/Input.py
@@ -0,0 +1,8 @@
+"""Pauses the program and waits for the user to type something.
+Returns the entered value as a string (str).
+Optionally displays a prompt message to guide the user."""
+
+x = input("Enter your name: ")
+print ("my name is ", x)
+print(f"Hello, {x}!")
+print(f"My name is {x}.")
--- Python Basics/Variable & type.py
+++ Python Basics/Variable & type.py
@@ -1,2 +1,5 @@
 name = input("Enter your name: ")
 print("Hello,", name, "! Welcome!")
+
+
+#How to Change the Type of Input in Python
